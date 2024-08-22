4f3eded5d592bc3b08a39c2ee343f8ad28a49f17
Kushal Dahal <kushal.dahal.234@gmail.com>
2024-08-22 12:43:25 +0000

    connect mongo

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
-from pymongo import MongoClient
+
+import asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+# from pymongo.server_api import ServerApi
+
 from fastapi import FastAPI, Request
 from typing import Union
 from dotenv import load_dotenv
@@ -9,8 +13,10 @@ from fastapi.templating import Jinja2Templates
 
 load_dotenv()
 
-MONGODB_URI = os.getenv("MONGODB_URI")
-conn = MongoClient(MONGODB_URI)
+MONGODB_URI = os.getenv("MONGODB_URI") #+ "&tlsInsecure=true"
+client = AsyncIOMotorClient(MONGODB_URI)#, server_api=ServerApi('1'))
+database = client.First
+collection = database.get_collection("First")
 
 app = FastAPI()
 
@@ -18,14 +24,24 @@ app.mount("/static", StaticFiles(directory = "static"), name='static')
 templates = Jinja2Templates(directory="templates")
 
 
+# Convert MongoDB document to Pydantic model
+# def document_to_model(document) -> MyModel:
+#     return MyModel(**document)
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
+    
     try:
-        conn.admin.command('ping')
+        client.admin.command('ping')
         constringr = "Pinged your deployment. You successfully connected to MongoDB!"
     except Exception as e:
         constringr = e
-    return templates.TemplateResponse("index.html",{"request":request,"res":constringr})
+
+    cursor = collection.find({})
+    #datas is list of dictionaries
+    datas = await cursor.to_list(length = 100)
+    docs = [{"id" : data["_id"],"note": data["note"]} for data in datas]
+    return templates.TemplateResponse("index.html",{"request":request,"res":constringr,"datas":docs})
     # return {"Hello": "World"}
 
 
