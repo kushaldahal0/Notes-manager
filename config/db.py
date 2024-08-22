53dc9c2f6d1e77ffe6a8800986a4f5675c5013bf
Kushal Dahal <kushal.dahal.234@gmail.com>
2024-08-22 13:33:15 +0000

    add config

---
+++ config/db.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+import asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+
+load_dotenv()
+
+MONGODB_URI = os.getenv("MONGODB_URI") #+ "&tlsInsecure=true"
+client = AsyncIOMotorClient(MONGODB_URI)#, server_api=ServerApi('1'))
+database = client.First
+collection = database.get_collection("First")
